102d9621203c0672569db46c4f4429615ff04d0a
John Kennedy Akabo <developer.akabojohnkennedy@gmail.com>
2020-07-23 16:10:32 +0000

    Added paginations to liststockadjustments and listsupplies

--- src/views/app/ListStockAdjustments.vue
+++ src/views/app/ListStockAdjustments.vue
@@ -17,6 +17,7 @@
                         </v-btn>
                     </template>
                 </v-data-table>
+                <v-pagination :length="dataTable.pageCount" v-if="dataTable.pageCount > 1" v-model="dataTable.page"/>
             </v-card>
         </v-flex>
         <add-stock-adjustment :active.sync="addDialog" @submit="$apollo.queries.stockAdjustments.refetch()"/>
